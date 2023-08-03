101c45f5ac04d4738fb72ec331839f4a7c4f6612
Heo <gjgudrn1114@naver.com>
2023-08-03 21:20:09 +0900

    mariadb와 연동, JPA 설정

---
+++ src/main/java/com/example/backend/entity/Item.java
@@ -0,0 +1,19 @@
+package com.example.backend.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+
+}
