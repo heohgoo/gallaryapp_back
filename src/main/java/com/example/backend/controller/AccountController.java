93ff9ab1d036ba979abfe98e14170ade63ea292c
Heo <gjgudrn1114@naver.com>
2023-08-06 19:38:27 +0900

    JWT로 쿠키 설정

--- src/main/java/com/example/backend/controller/AccountController.java
+++ src/main/java/com/example/backend/controller/AccountController.java
@@ -2,14 +2,20 @@ package com.example.backend.controller;
 
 import com.example.backend.entity.Member;
 import com.example.backend.repository.MemberRepository;
+import com.example.backend.service.JwtService;
+import com.example.backend.service.JwtServiceImpl;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @RestController
@@ -18,12 +24,23 @@ public class AccountController {
     MemberRepository memberRepository;
 
     @PostMapping("/api/account/login")
-    public int login(@RequestBody Map<String, String> params) {
+    public ResponseEntity login(@RequestBody Map<String, String> params,
+                                HttpServletResponse res) {
        Member member =
                memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
 
        if (member != null) {
-           return member.getId();
+           JwtService jwtService = new JwtServiceImpl();
+           int id = member.getId();
+           String token = jwtService.getToken("id", id);
+           //key, value
+
+           Cookie cookie = new Cookie("token", token);
+           cookie.setHttpOnly(true);
+           cookie.setPath("/");
+
+           res.addCookie(cookie);
+           return new ResponseEntity<>(id, HttpStatus.OK);
        }
 
        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
